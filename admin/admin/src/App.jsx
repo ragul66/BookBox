9e962aa29919bf041a846a0743214a90689941df
ragul66 <114981373+ragul66@users.noreply.github.com>
2024-01-26 00:03:50 +0530

    commit

--- admin/admin/src/App.jsx
+++ admin/admin/src/App.jsx
@@ -15,7 +15,6 @@ function App() {
           <Route path='/AdminDB' element={<AdDb />} />
           <Route path='/AdminEdit' element={<AdEdit />} />
           <Route path='/home' element={<AdDb />} />
-          <Route path='/data' element={<AdEdit />} />
         </Routes>
       </Router>
     </>
