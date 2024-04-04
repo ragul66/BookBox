7aa7253755b43dc9112e2e7ab54371d625f26c9b
ragul66 <114981373+ragul66@users.noreply.github.com>
2024-04-04 18:16:50 +0530

    adding ratings

--- client/src/components/CategoryCarousel.jsx
+++ client/src/components/CategoryCarousel.jsx
@@ -14,30 +14,32 @@ const Card2 = () => {
     };
 
     return (
-        <div className="container bg-orange-50 p-4 font-serif">
-            <div className="w-3/4  m-auto space-x-4">
-                <div className="mt-8">
-                    <Slider className="lg:w-[1300px] lg:-mt-6 lg:-ml-20 " {...settings}>
-                        {data.map((d) => (
-                            <div className="border-2 border-black  text-black rounded-lg">
+        <>
+            <div className="container bg-orange-50 p-4 font-serif">
+                <div className="w-3/4  m-auto space-x-4">
+                    <div className="mt-8">
+                        <Slider className="lg:w-[1300px] lg:-mt-6 lg:-ml-20 " {...settings}>
+                            {data.map((d) => (
+                                <div className="border-2 border-black  text-black rounded-lg">
 
-                                <div className="h-24 rounded-t-xl flex flex-row justify-center items-center">
-                                    <img src={d.img} alt="" className="h-28 w-28 rounded-full mt-12" />
+                                    <div className="h-24 rounded-t-xl flex flex-row justify-center items-center">
+                                        <img src={d.img} alt="" className="h-28 w-28 rounded-full mt-12" />
+                                    </div>
+
+                                    <div className="flex flex-col justify-center items-center gap-4 p-4 mt-6">
+                                        <p className="text-xl font-semibold">{d.name}</p>
+                                        <p className="items-center justify-center ml-8">{d.review}</p>
+                                        <h1>₹{d.price}</h1>
+                                        {/* <button className="border-3 bg-blue-400 rounded-xl p-2 text-white ">Read More</button> */}
+                                    </div>
+
                                 </div>
-
-                                <div className="flex flex-col justify-center items-center gap-4 p-4 mt-6">
-                                    <p className="text-xl font-semibold">{d.name}</p>
-                                    <p className="items-center justify-center ml-8">{d.review}</p>
-                                    <h1>₹{d.price}</h1>
-                                    {/* <button className="border-3 bg-blue-400 rounded-xl p-2 text-white ">Read More</button> */}
-                                </div>
-
-                            </div>
-                        ))}
-                    </Slider>
+                            ))}
+                        </Slider>
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     );
 };
 
